d66829d03ea8d27e6ed44d6e841f749ba4fb1450
HanSolem <brian.r.solem@gmail.com>
2022-10-18 21:37:02 -0500

    19.1

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,15 @@
+var helloWorld = "Hello World!";
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+    console.log(five);
+}
+addNumbers();
+function stringConcat() {
+    var taco = "You're Not Allowed";
+    var cat = "To Do That, Dave";
+    var tacoCat = taco + " " + cat;
+    alert(tacoCat);
+}
+stringConcat();
